63e918f561527dc1a36eaadf5544b3a1635092a7
fjk0816 <2285262879@qq.com>
2020-09-09 17:00:14 +0800

    Revert "修改了Holle文件2"
    This reverts commit de311f65

--- git_01/src/main/java/Hello.java
+++ git_01/src/main/java/Hello.java
@@ -6,6 +6,5 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("aaaa");
         System.out.println("h1");
-        System.out.println("h123");
     }
 }
