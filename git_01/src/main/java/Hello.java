6f17626297e588dd1811289f65930516c47d8462
fjk0816 <2285262879@qq.com>
2020-09-09 16:48:21 +0800

    创建了一个项目

---
+++ git_01/src/main/java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public void hello(){
+        System.out.println("aaa");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
+}
