3faa57a8d32439e737c21a7146267810d14c10a6
fjk0816 <2285262879@qq.com>
2020-09-09 17:07:08 +0800

    Revert "Revert "修改第四次""
    This reverts commit f79ee11e

--- git_01/src/main/java/Hello.java
+++ git_01/src/main/java/Hello.java
@@ -7,6 +7,5 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("aaaa");
         System.out.println("h1");
-        System.out.println("h123");
     }
 }
